b2ca9ffe58854c5925c6791db1b7aa72c69c4dbb
James Newman <jamesmnewman1@gmail.com>
2016-10-28 21:01:18 -0700

    Working on my iphone

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,14 @@
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'angular-ionic-todo' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('todo', ['ionic', 'LocalStorageModule']);
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+app.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -17,64 +18,77 @@ app.run(function($ionicPlatform) {
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
 
 app.config(function (localStorageServiceProvider) {
-    localStorageServiceProvider
-      .setPrefix('todo');
-  })
+  localStorageServiceProvider
+    .setPrefix('angular-ionic-todo')
+});
 
-app.controller('main', function($scope, $ionicModal, localStorageService) {
+
+app.controller('main', function ($scope, $ionicModal, localStorageService) {
+  //store the entities name in a variable
+  var taskData = 'task';
+
+  // initialize the tasks scope with empty array
   $scope.tasks = [];
 
-  $scope.task = {};
+  //initialize the task scope with empty ojbect
+  $scope.task = [];
 
-  // configure the ionic modal
+  //configure the ionic model before use
   $ionicModal.fromTemplateUrl('new-task-modal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
+    scope: $scope,
+    animation: 'slide-in-up'
   }).then(function (modal) {
-      $scope.newTaskModal = modal;
+    $scope.newTaskModal = modal;
   });
 
-  $scope.getTasks = function() {
-    // fetches from local storage
+  $scope.getTasks = function () {
+    // fetches task from local storage
     if (localStorageService.get(taskData)) {
       $scope.tasks = localStorageService.get(taskData);
     } else {
       $scope.tasks = [];
     }
-  }
+  };
 
-  $scope.createTask = function() {
-    // creates a new task
+  $scope.createTask = function () {
+    //creates a new task
     $scope.tasks.push($scope.task);
     localStorageService.set(taskData, $scope.tasks);
     $scope.task = {};
+
     //close new task modal
     $scope.newTaskModal.hide();
-  }
+  };
 
-  $scope.removeTask = function(index) {
-    // destroys/removes a task
+  $scope.removeTask = function (index) {
+    //removes a task
     $scope.tasks.splice(index, 1);
     localStorageService.set(taskData, $scope.tasks);
-  }
+  };
 
-  $scope.completeTask = function(index) {
-    // sets task to complete
+  $scope.completeTask = function (index) {
+    //updates a task as completed
     if (index !== -1) {
       $scope.tasks[index].completed = true;
     }
+
     localStorageService.set(taskData, $scope.tasks);
-  }
-})
+  };
 
+  $scope.openTaskModal = function () {
+    $scope.newTaskModal.show();
+  };
 
+  $scope.closeTaskModal = function () {
+    $scope.newTaskModal.hide();
+  };
 
-
+});
 
